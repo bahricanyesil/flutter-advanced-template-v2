38bf1232fa2615e65c8d7403e5e8dca826cec541
bahricanyesil <bahricanyesil@gmail.com>
2024-08-22 02:52:23 +0300

    feature: create an abstract analytics manager implementation

--- packages/analytics_manager/README.md
+++ packages/analytics_manager/README.md
@@ -1,39 +1,35 @@
-<!--
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
+# Analytics Manager
 
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/tools/pub/writing-package-pages).
-
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/to/develop-packages).
--->
-
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
+The `analytics_manager` module provides a unified interface for managing analytics services in Flutter applications. This module is designed to facilitate the integration of various analytics platforms, with built-in support for Firebase Analytics.
 
 ## Features
 
-TODO: List what your package can do. Maybe include images, gifs, or videos.
+- **Log Events:** Track custom events in your app.
+- **Set User Properties:** Assign properties to users for more detailed analytics.
+- **Log Screen Views:** Record screen views to analyze user navigation.
 
-## Getting started
+## Installation
 
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
+To use the `analytics_manager` module in your Flutter project, add it to your `pubspec.yaml` file:
+
+```yaml
+dependencies:
+  analytics_manager:
+    path: ../packages/analytics_manager
+```
 
 ## Usage
 
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder.
+Here's a basic example of how to use the `analytics_manager` module:
+
+### Import the Module
 
 ```dart
-const like = 'sample';
+import 'package:analytics_manager/analytics_manager.dart';
 ```
 
-## Additional information
+### Initialize `AnalyticsManager`
 
-TODO: Tell users more about the package: where to find more information, how to
-contribute to the package, how to file issues, what response they can expect
-from the package authors, and more.
+```dart
+final IAnalyticsManager analyticsManager = IAnalyticsManager.instance;
+```
--- packages/analytics_manager/lib/analytics_manager.dart
+++ packages/analytics_manager/lib/analytics_manager.dart
@@ -1,8 +1,116 @@
-/// Analytics Manager Library
-library analytics_manager;
+import 'package:flutter/foundation.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import '../logger/base_log_manager.dart';
+
+/// Base class for analytics report manager
+abstract class IAnalyticsManager {
+  /// Create an instance of the analytics manager
+  const IAnalyticsManager(this._logManager);
+
+  /// Logger manager instance
+  final ILogManager _logManager;
+
+  /// Initialize the analytics manager
+  Future<void> init();
+
+  /// Dispose the analytics manager
+  void dispose();
+
+  /// Log an app open event
+  @mustCallSuper
+  Future<void> logAppOpen() async {
+    _logManager.lInfo('Analytics Reported - App Open');
+  }
+
+  /// Log an event
+  @mustCallSuper
+  Future<void> logEvent(
+    String name, {
+    Map<String, Object>? parameters,
+  }) async {
+    _logManager
+        .lInfo('Analytics Reported - Event: $name, Parameters: $parameters');
+  }
+
+  /// Log a screen view
+  @mustCallSuper
+  Future<void> logScreenView(
+    String name, {
+    Map<String, dynamic>? parameters,
+  }) async {
+    _logManager
+        .lInfo('Analytics Reported - Screen: $name, Parameters: $parameters');
+  }
+
+  /// Log a user login
+  @mustCallSuper
+  Future<void> onLogIn({
+    required String loginMethod,
+    Map<String, Object>? parameters,
+  }) async {
+    _logManager.lInfo('Analytics Reported - User Logged In: $loginMethod');
+  }
+
+  /// Log a user sign up
+  @mustCallSuper
+  Future<void> onSignUp({
+    required String signUpMethod,
+    Map<String, Object>? parameters,
+  }) async {
+    _logManager.lInfo('Analytics Reported - User Signed Up: $signUpMethod');
+  }
+
+  /// Set the user id
+  @mustCallSuper
+  Future<void> setUserId(String userId) async {
+    _logManager.lInfo('Analytics Reported - User ID: $userId');
+  }
+
+  /// Set the user property
+  @mustCallSuper
+  Future<void> setUserProperty({
+    required String name,
+    String? value,
+  }) async {
+    _logManager.lInfo('Analytics Reported - User Property: $name: $value');
+  }
+
+  /// Log a share event
+  @mustCallSuper
+  Future<void> onShare({
+    required String contentType,
+    required String itemId,
+    required String method,
+    Map<String, Object>? parameters,
+  }) async {
+    _logManager.lInfo(
+      '''Analytics Reported - Share: $contentType, Item: $itemId, Method: $method, Parameters: $parameters''',
+    );
+  }
+
+  /// Log a purchase event
+  @mustCallSuper
+  Future<void> logPurchase({
+    String? currency,
+    String? coupon,
+    double? value,
+    double? tax,
+    double? shipping,
+    String? transactionId,
+    String? affiliation,
+    Map<String, Object>? parameters,
+  }) async {
+    _logManager.lInfo(
+      '''Analytics Reported - Purchase: Currency: $currency, Coupon: $coupon, Value: $value, Tax: $tax, Shipping: $shipping, Transaction ID: $transactionId, Affiliation: $affiliation, Parameters: $parameters''',
+    );
+  }
+
+  /// Reset the analytics data
+  Future<void> resetAnalyticsData();
+
+  /// Enable analytics
+  Future<void> enableAnalytics();
+
+  /// Disable analytics
+  Future<void> disableAnalytics();
 }

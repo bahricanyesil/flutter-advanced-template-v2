bb22fe6d59d9869dc701078ffee4a741810142e1
bahricanyesil <bahricanyesil@gmail.com>
2024-08-24 01:13:50 +0300

    test: cover additional cases for custom pretty printer

--- packages/log_manager/test/utils/custom_pretty_printer_test.dart
+++ packages/log_manager/test/utils/custom_pretty_printer_test.dart
@@ -22,7 +22,6 @@ void main() {
       expect(printer.lineLength, 100);
       expect(printer.printColors, true);
       expect(printer.printEmojis, true);
-      expect(printer.printTime, false);
       expect(printer.excludeBox, <LogLevel, bool>{});
       expect(printer.noBoxingByDefault, false);
       expect(printer.excludePaths, <String>[]);
@@ -40,7 +39,8 @@ void main() {
 
       final String formatted = printer.log(logEvent).join('\n');
 
-      // Verify that the message is formatted correctly without colors and emojis
+      // Verify that the message is formatted correctly
+      //without colors and emojis
       expect(formatted, contains('Test message'));
       expect(formatted, isNot(contains('🐛')));
     });
@@ -61,7 +61,8 @@ void main() {
       );
 
       final String formatted = printer.log(logEvent).join('\n');
-      // Adjust the test to check for the presence of stack trace lines within the formatted output
+      // Adjust the test to check for the presence of stack trace lines
+      // within the formatted output
       expect(
         formatted,
         contains('#0main (package:my_app/main.dart:10:10)'),
@@ -118,12 +119,32 @@ void main() {
       final String outputRes = printer.log(logEvent).join('\n');
       final String formattedOutput = _stripAnsiCodes(outputRes);
       const JsonEncoder encoder = JsonEncoder.withIndent('  ');
-
       final String encodedMessage = encoder.convert(message);
-      final String expected = encodedMessage;
       final String removedBorderOutput = formattedOutput.replaceAll('│ ', '');
 
-      expect(removedBorderOutput, contains(expected));
+      expect(removedBorderOutput, contains(encodedMessage));
+    });
+
+    test('should format JSON in message correctly', () {
+      final Map<String, dynamic> message = <String, dynamic>{'key': 'value'};
+      final LogEvent logEvent = LogEvent(
+        Level.debug,
+        message,
+        error: Exception('test exception'),
+        time: DateTime.now(),
+      );
+
+      final String outputRes = printer.log(logEvent).join('\n');
+      final String formattedOutput = _stripAnsiCodes(outputRes);
+      const JsonEncoder encoder = JsonEncoder.withIndent('  ');
+
+      final String encodedMessage = encoder.convert(message);
+      final StringBuffer expectedBuffer = StringBuffer();
+      for (final String line in encodedMessage.split('\n')) {
+        expectedBuffer.writeln('│ 🐛 $line');
+      }
+
+      expect(formattedOutput, contains(expectedBuffer.toString()));
     });
 
     test('should exclude paths based on excludePaths', () {
@@ -154,9 +175,63 @@ void main() {
       final String formatted1 = printer.log(logEvent1).join('\n');
       final String formatted2 = printer.log(logEvent2).join('\n');
 
-      // Check if the formatted output contains or excludes based on excludePaths
+      // Check if the formatted output contains or
+      //excludes based on excludePaths
       expect(formatted1, isNot(contains('package:logger/src/logger.dart')));
       expect(formatted2, contains('other_package/src/logger.dart'));
+    });
+
+    test('should handle error formatting correctly', () {
+      final LogEvent logEvent = LogEvent(
+        Level.error,
+        'test message',
+        error: Exception('test exception'),
+      );
+
+      final List<String> output = printer.log(logEvent);
+
+      // Check if the output includes the string representation of the error
+      expect(output.join('\n'), contains('Exception: test exception'));
+    });
+    test('should format stack trace correctly, filtering web stack trace lines',
+        () {
+      // Create a log event with a sample stack trace
+      final LogEvent logEvent = LogEvent(
+        Level.error,
+        'Test error message',
+        stackTrace: StackTrace.fromString(
+            'packages/another_package/another_file.dart:101\n'
+            'package:log_manager/src/utils/custom_pretty_printer.dart:202\n'),
+      );
+
+      // Get the log output
+      final List<String> logOutput = printer.log(logEvent);
+
+      // Join lines for comparison
+      final String formattedOutput = logOutput.join('\n');
+
+      // Define discarded lines that should not be present in the output
+      const String discardedLine1 = 'dart-sdk/lib/some_lib.dart:456';
+      const String discardedLine2 =
+          'packages/another_package/another_file.dart:101';
+
+      // Assert that discarded lines are not present
+      expect(formattedOutput, isNot(contains(discardedLine1)));
+      expect(formattedOutput, isNot(contains(discardedLine2)));
+    });
+
+    test('should use _toEncodableFallback when encoding JSON', () {
+      final dynamic message = <String, String Function()>{
+        'key': () => 'value',
+      }; // Use a non-serializable object to trigger fallback
+
+      // Log a message to trigger _stringifyMessage
+      final List<String> logOutput =
+          printer.log(LogEvent(Level.debug, message));
+      final String formattedOutput = logOutput.join('\n');
+      const String expected = '''│ 🐛   "key": "Closure: () => String"''';
+
+      expect(formattedOutput, contains(expected));
     });
   });
 
@@ -164,7 +239,8 @@ void main() {
     test('should inherit properties from CustomPrettyPrinter', () {
       final DefaultPrettyPrinter printer = DefaultPrettyPrinter();
 
-      // DefaultPrettyPrinter should inherit the default values from CustomPrettyPrinter
+      // DefaultPrettyPrinter should inherit the default values
+      //from CustomPrettyPrinter
       expect(printer.printTime, false);
       expect(printer.onlyErrorStackTrace, true);
     });

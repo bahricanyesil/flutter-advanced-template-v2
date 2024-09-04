5ee57e161832c507581b3f4a33256e3048c980c5
bahricanyesil <bahricanyesil@gmail.com>
2024-09-04 17:28:40 +0300

    feat: create base file and folders

---
+++ packages/connectivity_manager/lib/connectivity_manager.dart
@@ -0,0 +1,5 @@
+/// Library for connectivity manager that
+/// provides a unified interface for monitoring network connectivity.
+library connectivity_manager;
+
+export 'src/connectivity_manager.dart';

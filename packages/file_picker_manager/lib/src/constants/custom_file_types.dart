bc0be91814e9a9c462d4e61ee79d4d413972b4c7
bahricanyesil <bahricanyesil@gmail.com>
2024-09-04 22:08:29 +0300

    feat: implement file picker manager interface

---
+++ packages/file_picker_manager/lib/src/constants/custom_file_types.dart
@@ -0,0 +1,20 @@
+/// Custom file types for file picker
+enum CustomFileTypes {
+  /// Any file type
+  any,
+
+  /// Media file type
+  media,
+
+  /// Image file type
+  image,
+
+  /// Video file type
+  video,
+
+  /// Audio file type
+  audio,
+
+  /// Custom file type
+  custom,
+}
--- packages/file_picker_manager/lib/src/file_picker_manager.dart
+++ packages/file_picker_manager/lib/src/file_picker_manager.dart
@@ -1,1 +1,14 @@
+import 'dart:io';
 
+import 'constants/custom_file_types.dart';
+
+/// An abstract class that provides a contract for file picker operations.
+// ignore: one_member_abstracts
+abstract interface class FilePickerManager {
+  /// Picks an file from the user's device.
+  Future<File?> pickFile({
+    List<String> allowedExtensions = const <String>[],
+    bool allowMultiple = false,
+    CustomFileTypes? type,
+  });
+}

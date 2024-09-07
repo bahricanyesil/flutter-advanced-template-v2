715c24f5dddd7e6b8ae29658298da0dfa6fd0205
bahricanyesil <bahricanyesil@gmail.com>
2024-09-07 22:42:26 +0300

    feat: create base response model and interface

---
+++ packages/local_notification_manager/lib/src/local_notification_manager.dart
@@ -0,0 +1,50 @@
+import 'models/custom_notification_response_model.dart';
+
+/// Abstract class for the local notification manager.
+abstract interface class LocalNotificationManager {
+  /// Initializes the local notification manager.
+  Future<void> initialize();
+
+  /// Shows an instant notification.
+  Future<void> showNotification({
+    required int id,
+    required String title,
+    required String body,
+    String? payload,
+  });
+
+  /// Schedules a notification.
+  Future<void> scheduleNotification({
+    required int id,
+    required String title,
+    required String body,
+    required DateTime scheduledDate,
+    String? payload,
+  });
+
+  /// Cancels a notification.
+  Future<void> cancelNotification(int id);
+
+  /// Cancels all notifications.
+  Future<void> cancelAllNotifications();
+
+  /// On did receive local notification callback.
+  void onDidReceiveLocalNotification(
+    int id,
+    String? title,
+    String? body,
+    String? payload,
+  );
+
+  /// On did receive notification response callback.
+  void onDidReceiveNotificationResponse(
+      CustomNotificationResponseModel response);
+
+  /// On did receive background notification response callback.
+  void onDidReceiveBackgroundNotificationResponse(
+    CustomNotificationResponseModel response,
+  );
+
+  /// Whether notifications enabled.
+  static const String storageKey = 'notifications_enabled';
+}

dca372560f1774502b9f3c630e0f2832fdeea6bb
bahricanyesil <bahricanyesil@gmail.com>
2024-09-08 23:11:11 +0300

    feat: create manager abstract class

--- packages/key_value_storage_manager/lib/src/key_value_storage_manager.dart
+++ packages/key_value_storage_manager/lib/src/key_value_storage_manager.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/foundation.dart';
 
+import 'models/data_model.dart';
+
+/// The base class for the key-value storage manager.
+@immutable
+abstract interface class KeyValueStorageManager {
+  /// The list of supported types.
+  List<Type> get supportedTypes;
+
+  /// Writes the value to the storage.
+  Future<bool> write<T>({
+    required String key,
+    required T value,
+    bool defaultToString = false,
+  });
+
+  /// Reads the value from the storage.
+  T? read<T>(String key, {bool tryToParse = false});
+
+  /// Deletes the value from the storage.
+  Future<bool> delete({required String key});
+
+  /// Deletes all the values from the storage.
+  Future<bool> deleteAll();
+
+  /// Checks if the key is present in the storage.
+  bool containsKey(String key);
+
+  /// Reads the list of models from the storage.
+  List<T>? readModelList<T extends DataModel<T>>(String key);
+}

f7ab70bca39b08669b025b4a579115773eb871cc
bahricanyesil <bahricanyesil@gmail.com>
2024-09-08 13:04:36 +0300

    feat: adjust auth manager library exports

--- packages/auth_manager/lib/auth_manager.dart
+++ packages/auth_manager/lib/auth_manager.dart
@@ -1,1 +1,9 @@
+/// Library for authentication manager that
+/// provides a unified interface for authentication.
+library auth_manager;
 
+export 'src/auth_manager.dart';
+export 'src/entities/auth_result_entity.dart';
+export 'src/entities/user_entity.dart';
+export 'src/extensions/user_entity_extensions.dart';
+export 'src/firebase_auth_manager.dart';
--- packages/local_notification_manager/lib/local_notification_manager.dart
+++ packages/local_notification_manager/lib/local_notification_manager.dart
@@ -2,5 +2,12 @@
 /// provides a unified interface for managing local notifications.
 library local_notification_manager;
 
+export 'src/enums/custom_notification_visibility.dart';
+export 'src/enums/notification_category.dart';
+export 'src/enums/notification_importance.dart';
+export 'src/enums/notification_interruption_level.dart';
+export 'src/enums/notification_priority.dart';
 export 'src/local_notification_manager.dart';
 export 'src/local_notification_manager_impl.dart';
+export 'src/models/custom_local_notification_settings.dart';
+export 'src/models/custom_notification_response_model.dart';

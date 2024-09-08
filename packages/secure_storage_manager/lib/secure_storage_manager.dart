2131845b8d884dd4a624b53186faea2dc69c1489
bahricanyesil <bahricanyesil@gmail.com>
2024-09-09 02:00:10 +0300

    feat: create base files and folders

---
+++ packages/secure_storage_manager/lib/secure_storage_manager.dart
@@ -0,0 +1,6 @@
+/// Library for secure storage manager that
+/// provides a unified interface for managing secure storage operations.
+library secure_storage_manager;
+
+export 'src/secure_storage_manager.dart';
+export 'src/secure_storage_manager_impl.dart';
---
+++ packages/secure_storage_manager/test/mocks/mock_log_manager.dart
@@ -0,0 +1,4 @@
+import 'package:log_manager/log_manager.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockLogManager extends Mock implements LogManager {}

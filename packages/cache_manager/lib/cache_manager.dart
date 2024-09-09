0291304f71ea4c5c667bfe5ce43966e7b18dcc3e
bahricanyesil <bahricanyesil@gmail.com>
2024-09-09 09:25:33 +0300

    feat: base files and folders like readme

---
+++ packages/cache_manager/lib/cache_manager.dart
@@ -0,0 +1,6 @@
+/// Library for cache manager that
+/// provides a unified interface for managing cache operations.
+library cache_manager;
+
+export 'src/cache_manager.dart';
+export 'src/cache_manager_impl.dart';

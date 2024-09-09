1206c48f938ad569ac14273b49d30c58ac092abf
bahricanyesil <bahricanyesil@gmail.com>
2024-09-09 13:09:37 +0300

    feat: create readme, license, and base files

---
+++ packages/network_manager/lib/network_manager.dart
@@ -0,0 +1,4 @@
+library network_manager;
+
+export 'src/network_manager.dart';
+export 'src/network_manager_impl.dart';
---
+++ packages/network_manager/test/network_manager_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('NetworkManager', () {
+    test('initialization', () {
+      // TODO: Write tests
+    });
+  });
+}

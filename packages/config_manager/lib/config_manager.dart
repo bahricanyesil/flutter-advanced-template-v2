d06ac179c43d4f8bab378733c28cfb473477cba6
Bahrican Yesil <bahricanyesil@gmail.com>
2024-09-16 00:34:22 +0300

    feat: Integrate flavor types and env (#51)

---
+++ packages/config_manager/lib/config_manager.dart
@@ -0,0 +1,10 @@
+/// A library to manage configurations for different environments in Flutter.
+///
+/// This package provides a way to load and manage configuration
+/// settings from env files. allowing for easy customization
+/// based on different build flavors.
+library config_manager;
+
+export 'src/config_manager.dart';
+export 'src/enums/flavor_types.dart';
+export 'src/env/base_env.dart';

b550d4988248873ca85d958cac0412ca2f512910
bahricanyesil <bahricanyesil@gmail.com>
2024-09-15 09:44:50 +0300

    feat: add required permissions to README

--- packages/network_manager/lib/network_manager.dart
+++ packages/network_manager/lib/network_manager.dart
@@ -7,5 +7,9 @@ library network_manager;
 
 import 'package:dart_mappable/dart_mappable.dart';
 
+export 'src/entities/token_entity.dart';
+export 'src/enums/method_types.dart';
+export 'src/enums/token_types.dart';
 export 'src/network_manager.dart';
 export 'src/network_manager_impl.dart';
+export 'src/utils/status_code_helpers.dart';

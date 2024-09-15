9bae12a436da1bed61d348968940a7c49a5de13b
bahricanyesil <bahricanyesil@gmail.com>
2024-09-15 23:08:34 +0300

    refactor: Remove deprecated AppThemeData class

--- packages/theme_manager/lib/src/models/theme_model.dart
+++ packages/theme_manager/lib/src/models/theme_model.dart
@@ -1,16 +1,25 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
-
-import 'app_theme_data.dart';
 
 /// ThemeModel is a class that holds the theme data for the app.
 @immutable
-base class ThemeModel {
+base class ThemeModel extends Equatable {
   /// ThemeModel is a class that holds the theme data for the app.
-  const ThemeModel({required this.name, required this.appThemeData});
+  const ThemeModel({
+    required this.name,
+    required this.lightTheme,
+    required this.darkTheme,
+  });
 
   /// Name of the theme.
   final String name;
 
-  /// Theme data for the app.
-  final AppThemeData appThemeData;
+  /// Light theme data.
+  final ThemeData lightTheme;
+
+  /// Dark theme data.
+  final ThemeData darkTheme;
+
+  @override
+  List<Object?> get props => <Object?>[name, lightTheme, darkTheme];
 }

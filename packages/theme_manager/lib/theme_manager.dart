fec990562407b754fec676dc61b2c59bd917b24f
bahricanyesil <bahricanyesil@gmail.com>
2024-09-15 23:04:24 +0300

    feat: Add ThemeBloc to manage theme state

--- packages/theme_manager/lib/theme_manager.dart
+++ packages/theme_manager/lib/theme_manager.dart
@@ -4,6 +4,9 @@
 /// theme mode to the database and restore it when the app is opened.
 library theme_manager;
 
+export 'src/bloc/theme_bloc.dart';
+export 'src/bloc/theme_event.dart';
+export 'src/bloc/theme_state.dart';
 export 'src/models/theme_model.dart';
 export 'src/theme_manager.dart';
 export 'src/theme_manager_impl.dart';

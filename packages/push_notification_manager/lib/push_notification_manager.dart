868bc9e2751c6b5d4652bdde1a5c82c3e61b35ca
bahricanyesil <bahricanyesil@gmail.com>
2024-09-16 23:56:18 +0300

    feat: export firebase push notification manager1

--- packages/push_notification_manager/lib/push_notification_manager.dart
+++ packages/push_notification_manager/lib/push_notification_manager.dart
@@ -2,4 +2,6 @@
 /// provides a unified interface for managing push notifications.
 library push_notification_manager;
 
+export 'src/firebase_push_notification_manager.dart';
 export 'src/push_notification_manager.dart';
+export 'src/utils/remote_message_extensions.dart';

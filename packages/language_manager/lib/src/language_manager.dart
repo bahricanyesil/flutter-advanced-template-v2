cf4a0ddb46d6db07a1bc52f381cfa159217000c1
bahricanyesil <bahricanyesil@gmail.com>
2024-11-22 23:03:19 +0300

    fix: remove protected annotation

--- packages/language_manager/lib/src/language_manager.dart
+++ packages/language_manager/lib/src/language_manager.dart
@@ -90,7 +90,6 @@ abstract class LanguageManager {
   }
 
   /// Checks if two locales are equal.
-  @protected
   static bool isLocaleEqual(Locale locale1, Locale locale2) =>
       locale1.languageCode == locale2.languageCode &&
       (locale1.countryCode ?? locale2.countryCode) ==

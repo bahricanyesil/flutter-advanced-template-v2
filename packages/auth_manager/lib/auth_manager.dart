e70432e29be32517af6f7f841fb9d149a718cec9
bahricanyesil <bahricanyesil@gmail.com>
2024-11-25 18:56:30 +0300

    feat: update auth manager exports

--- packages/auth_manager/lib/auth_manager.dart
+++ packages/auth_manager/lib/auth_manager.dart
@@ -5,5 +5,6 @@ library auth_manager;
 export 'src/auth_manager.dart';
 export 'src/entities/auth_result_entity.dart';
 export 'src/entities/user_entity.dart';
+export 'src/enum/auth_error_type.dart';
 export 'src/extensions/user_entity_extensions.dart';
 export 'src/firebase_auth_manager.dart';

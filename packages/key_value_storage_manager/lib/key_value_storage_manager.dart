f3dfafe274d25c21d82acfc6996c0228fd2a1b9a
bahricanyesil <bahricanyesil@gmail.com>
2024-12-03 01:37:22 +0300

    fix: export base data model fro local storage

--- packages/key_value_storage_manager/lib/key_value_storage_manager.dart
+++ packages/key_value_storage_manager/lib/key_value_storage_manager.dart
@@ -3,5 +3,6 @@
 library key_value_storage_manager;
 
 export 'src/key_value_storage_manager.dart';
+export 'src/models/base_data_model.dart';
 export 'src/shared_preferences_manager.dart';
 export 'src/utils/data_parser_helpers.dart';
